3ca4d51bef6679b9a27c9e46092a48ce87cfe700
Jose Wilfrido Fernandez Villalba <josewilfer@gmail.com>
2024-12-13 19:37:33 -0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+#Ask the user for their name
+name = input("Whats your name? ")
+
+#Remove whitespace from the str and capitalize each word of the str
+name = name.strip().title()
+
+#Split user name into first name and last name 
+parts = name.split()
+first, last = parts[0], parts[1] if len(parts) > 1 else ""
+
+#Show a message saying hello to the user
+print(f"Hello , {first} {last}") 
