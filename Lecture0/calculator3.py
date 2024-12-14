73f2b7c241f658d9bce3cf3979043022391897f8
Jose Wilfrido Fernandez Villalba <josewilfer@gmail.com>
2024-12-13 21:18:40 -0300

    add todo

---
+++ Lecture0/calculator3.py
@@ -0,0 +1,9 @@
+#Ask the user to declare two numbers
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+#Sum the numbers given by the user
+z = x / y
+
+#Print the sum result
+print(f"{z:.2f}")
